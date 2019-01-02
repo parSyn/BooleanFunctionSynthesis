96b9e029909fd57b7dec195313e8b1251c7fe420
Shetal <shetals@cse.iitb.ac.in>
2019-01-02 12:19:26 +0530

    Script to run parSyn.

--- scripts/parSyn.sh
+++ scripts/parSyn.sh
@@ -2,7 +2,7 @@
 
 #Usage
  
-if [ $# -lt 2 ]
+if [ $# -ne 2 ]
 then
 #./parSyn.sh aigFileName OrderFileName
     echo "Usage ./parSyn.sh AigOrVerilogFileName OrderFileName"
@@ -12,7 +12,7 @@ fi
 aigFile=$1
 orderFile=$2
 export LD_LIBRARY_PATH=$HOME/abc.latest/abc:$HOME/boost_1_68_0/stage/lib:$LD_LIBRARY_PATH
-export PATH=$HOME/GitRep/bin:$PATH
+export PATH=../bin:$PATH
 numCores=10
 
 mpiexec -np 10 parSyn $aigFile $orderFile 2 $orderFile 0 1 0
